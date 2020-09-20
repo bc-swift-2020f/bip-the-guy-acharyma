9a59db3897dd96ef22fadb07b6129e913740b9ee
Manogya Acharya <acharyma@bc.edu>
2020-09-20 17:07:56 -0400

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Manogya Acharya on 9/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
